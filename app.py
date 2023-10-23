4be5c2a19032da5cadae7d9043acecf61ce447c3
BestRazer <65088083+BestRazer@users.noreply.github.com>
2023-10-23 17:57:25 +0200

    mehrere dateien fix 2

--- app.py
+++ app.py
@@ -173,6 +173,7 @@ def upload_image(subdomain, files):
     if not os.path.isdir(imagepath):
         os.mkdir(imagepath)
     # Für jede Datei in der Liste
+    files = request.files.getlist('files[]') # Eine Liste von Datei-ähnlichen Objekten
     for file in files:
         path = imagepath + '/' + file.filename
         print("Dateipfad: ", path)
